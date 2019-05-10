af8cf1d8ceca3822d6d880fdf4b74778090be44d
John Simonsen <johnsimonsen@weber.edu>
2019-05-10 15:02:52 -0600

    helpful output to match java class

--- main.py
+++ main.py
@@ -1,17 +1,20 @@
 import pytest
 #update with assignment name
-import calculator
+import calculator as student
 
 def main():
     a = ''
     while a not in ('run','test'):
-        print("Choose an option:\n\tRun\tTest")
+        print("Choose an option:\n\tRun\n\tTest")
         a = input("Your choice: ")
         a=a.lower()
     if a=="run":
-        calculator.main()
+        print("\n--Running Program--\n")
+        student.main()
     elif a=="test":
+        print("\n--Testing Program--\n")
         pytest.main(['-v'])
 
 if __name__ == '__main__':
     main()
+    print("\n--Operation Complete--\n")
